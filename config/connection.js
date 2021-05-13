3a5d4a4dd7011d9a53c1f758a3f33080d1e520b2
funkcake <castobrianj@gmail.com>
2021-05-13 18:44:14 -0500

    14.2.5

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize("newsfeed_db", "root", "Fu11stack$Boy", {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,4 +39,7 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 module.exports = router;
